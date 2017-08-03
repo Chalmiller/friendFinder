ba149c75dd433ba405df0135bbb49fc3998ac78d
Christopher Miller <christopher@transcriptic.com>
2017-08-02 21:30:17 -0700

    Finished App

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+var friendsData=[{
+	"name" : "Chris",
+	"scores" : [5,4,3,5,1,3,2,1,3,5]
+},{
+	"name" : "Jackie",
+	"scores" : [1,4,3,5,1,1,2,1,2,3]
+},
+{
+	"name" : "Tom",
+	"scores" : [2,3,4,5,1,2,5,2,1,4]
+},
+{
+	"name" : "Nick",
+	"scores" : [2,3,4,5,1,1,5,2,5,4]
+},
+ {
+	"name" : "Ian",
+	"scores" : [2,3,4,5,1,4,5,2,5,4]
+
+	}];
+
+module.exports = friendsData;
