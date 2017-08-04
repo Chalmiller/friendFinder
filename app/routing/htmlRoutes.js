f6835811c3c32ac705e82ca0ee526d599e6b8b05
Christopher Miller <christopher@transcriptic.com>
2017-08-03 18:13:05 -0700

    Refactored a bit

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,17 +3,12 @@ var express = require("express");
 
 module.exports = function(app) {
     app.get("/survey", function(req, res) {
-        // body...
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
-
     app.use("/", function(req, res) {
-        // body...
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
-
     app.use('/css', function (req, res) {
-    	// body...
     	res.sendFile(path.join(__dirname, "../public/assets/css/style.css"));
     })
 }
