514002f23e66dfd0650e27506b76e23868d7ab70
Christopher Miller <christopher@transcriptic.com>
2017-08-03 20:01:09 -0700

    Refactored

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@ var friendsData=[{
 	"name" : "Chris",
 	"scores" : [5,4,3,5,1,3,2,1,3,5]
 },{
-	"name" : "Jackie",
+	"name" : "Hnin",
 	"scores" : [1,4,3,5,1,1,2,1,2,3]
 },
 {
@@ -10,7 +10,7 @@ var friendsData=[{
 	"scores" : [2,3,4,5,1,2,5,2,1,4]
 },
 {
-	"name" : "Nick",
+	"name" : "Matt",
 	"scores" : [2,3,4,5,1,1,5,2,5,4]
 },
  {
